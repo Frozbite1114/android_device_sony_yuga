ad4ef16990085b880344e90e786cdbc0fd0ec77a
Mahesh <mahesh140494@gmail.com>
2016-10-31 15:57:32 +0000

    yuga: Add AOSP-OMS support
    Change-Id: I78ad34484024036fee30a254a9acfc0182049346

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,5 @@
 #
-# Copyright 2013 The Android Open Source Project
+# Copyright (C) 2013 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,7 +14,5 @@
 # limitations under the License.
 #
 
-PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_yuga.mk \
-    $(LOCAL_DIR)/full_yuga.mk
-
+add_lunch_combo aosp_yuga-eng
+add_lunch_combo aosp_yuga-userdebug
